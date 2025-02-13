8c2d608b048ed2b3dd65cce54f8389713728c9af
damian-bots <tosu0791@gmail.com>
2025-02-14 03:39:18 +0530

    Update edit.py

--- edit.py
+++ edit.py
@@ -249,7 +249,6 @@ async def delete_media(context: ContextTypes.DEFAULT_TYPE):
     job = context.job
     try:
         await context.bot.delete_message(job.chat_id, job.data)
-        await context.bot.send_message(job.chat_id, f"⚠️ Media has been deleted!")
     except Exception as e:
         print(f"Failed to delete message {job.data}: {e}")
 
